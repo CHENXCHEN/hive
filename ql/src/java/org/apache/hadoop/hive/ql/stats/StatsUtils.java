e4ccbc5bb5cc4735aa19243610f2408790a4ef05
报时 <baoshi@fordeal.com>
2020-10-25 17:35:18 +0800

    fix exception: Size requested for unknown type

--- ql/src/java/org/apache/hadoop/hive/ql/stats/StatsUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/stats/StatsUtils.java
@@ -1051,7 +1051,8 @@ public class StatsUtils {
     } else if (colTypeLowerCase.equals(serdeConstants.INTERVAL_DAY_TIME_TYPE_NAME)) {
       return JavaDataModel.JAVA32_META;
     } else {
-      throw new IllegalArgumentException("Size requested for unknown type: " + colType);
+//      throw new IllegalArgumentException("Size requested for unknown type: " + colType);
+      return 0;
     }
   }
 
